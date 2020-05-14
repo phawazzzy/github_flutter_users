636432e82a309591f39560b6cf90e59c52fca750
kareem fawas <phawazzzy@gmail.com>
2020-05-14 15:41:19 +0100

    turned to design to flutter code and completed UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,65 +10,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Github users',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(
+          'Github Users',
+          style: TextStyle(fontFamily: 'nunito'),
         ),
+        backgroundColor: Color(0xFF2F3035),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: UsersList(),
+    );
+  }
+}
+
+class UsersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 25.0,
+                    backgroundImage: NetworkImage(
+                        'https://avatars1.githubusercontent.com/u/36922198?s=460&u=2f6c001435568e44b18fc5f657062532dcf92758&v=4'),
+                  ),
+                  title: Text(
+                    'name of user',
+                    style: TextStyle(fontSize: 20.0, fontFamily: 'nunito', fontWeight: FontWeight.bold,),
+                  ),
+                  subtitle: Text('user location'),
+                  trailing: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(
+                          color: Colors.black,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:8.0, right: 8.0, top: 5.0, bottom: 5.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text("View Profile", style: TextStyle(fontSize: 10.0),),
+                            SizedBox(width: 5.0,),
+                            Icon(FontAwesomeIcons.github, size: 15.0,),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(height: 20.0, color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
